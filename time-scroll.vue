347c371fc8a708b6d2186115a520037cc4cb9279
西西 <1115279489@qq.com>
2020-09-17 20:36:58 +0800

    日期滚动显示, 可精确到分钟

--- time-scroll.vue
+++ time-scroll.vue
@@ -1,9 +1,6 @@
 <template>
     <view>
         <view class="uni-padding-wrap">
-			<view class="uni-title">
-				日期：{{year}}年{{month}}月{{day}}日
-			</view>
 		</view>
         <picker-view v-if="visible" :indicator-style="indicatorStyle" :value="value" @change="bindChange">
             <picker-view-column>
@@ -15,7 +12,16 @@
             <picker-view-column>
                 <view class="time-item" v-for="(item,index) in days" :key="index">{{item}}日</view>
             </picker-view-column>
+			<picker-view-column>
+			    <view class="time-item" v-for="(item,index) in hours" :key="index">{{item}}时</view>
+			</picker-view-column>
+			<picker-view-column>
+			    <view class="time-item" v-for="(item,index) in minutes" :key="index">{{item}}分</view>
+			</picker-view-column>
         </picker-view>
+		<view class="uni-title">
+			{{year}}年{{month}}月{{day}}日{{hour}}时{{minute}}分
+		</view>
     </view>
 </template>
 
@@ -29,15 +35,37 @@
             const month = date.getMonth() + 1;
             const days = [];
             const day = date.getDate();
+			const hours = [];
+			const hour = date.getHours();
+			const minutes = [];
+			const minute = date.getMinutes();
             for (let i = 1990; i <= date.getFullYear(); i++) {
                 years.push(i);
             }
             for (let i = 1; i <= 12; i++) {
+				if (i < 10) {
+					i = "0" + i;
+				}
                 months.push(i);
             }
             for (let i = 1; i <= 31; i++) {
+				if (i < 10) {
+					i = "0" + i;
+				}
                 days.push(i);
             }
+			for (let i = 0; i <= 23; i++) {
+				if (i < 10) {
+					i = "0" + i;
+				}
+			    hours.push(i);
+			}
+			for (let i = 0; i <= 59; i++) {
+				if (i < 10) {
+					i = "0" + i;
+				}
+			    minutes.push(i);
+			}
             return {
                 years,
                 year,
@@ -45,7 +73,11 @@
                 month,
                 days,
                 day,
-                value: [9999, month - 1, day - 1],
+				hours,
+				hour,
+				minutes,
+				minute,
+                value: [9999, month - 1, day - 1, hour - 1, minute - 1],
 				/**
 				 * 解决动态设置indicator-style不生效的问题
 				 */
@@ -63,6 +95,8 @@
                 this.year = this.years[val[0]];
                 this.month = this.months[val[1]];
                 this.day = this.days[val[2]];
+				this.hour = this.hours[val[3]];
+				this.minute = this.minutes[val[4]];
 				this.days = [];
 				if (this.year % 4 == 0 && ( this.year % 100 !=0 || this.year % 400 == 0) && this.month == 2) {
 					for (let i = 1; i <= 29; i++) {
@@ -104,11 +138,15 @@
 <style>
     picker-view {
         width: 100%;
-        height: 600rpx;
-        margin-top: 20rpx;
+        height: 300rpx;
+		margin-left: 20rpx;
     }
     .item {
         line-height: 100rpx;
         text-align: center;
     }
+	.uni-title {
+		margin-left: 100rpx;
+		margin-top: 20rpx;
+	}
 </style>
